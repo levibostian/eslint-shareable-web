21e6914518b041352ddafc77517a2473a6de3123
rskrz <ryanskrzypek@gmail.com>
2020-04-15 17:59:30 -0500

    feat: add typescript rules

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,2 +1,4 @@
 const foo: any = "";
 console.log(foo);
+const CamelCased = "abc";
+console.log(CamelCased);
--- index.js
+++ index.js
@@ -6,6 +6,12 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "semi": "off",
+    "no-process-env": "error",
+    "no-await-in-loop": "error",
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/no-namespace": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-unused-vars": [
       "error",
       {
@@ -20,6 +26,56 @@ module.exports = {
       }
     ],
     "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/no-empty-function": "off"
-  }
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/explicit-member-accessibility": [
+      "error",
+      {
+        "accessibility": "no-public",
+        "overrides": {
+          "parameterProperties": "off",
+          "properties": "explicit"
+        }
+      }
+    ],
+    "@typescript-eslint/no-parameter-properties": "off",
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
+      {
+        "multiline": {
+          "delimiter": "none",
+          "requireLast": true
+        },
+        "singleline": {
+          "delimiter": "semi",
+          "requireLast": false
+        }
+      }
+    ],
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      {
+        "functions": true,
+        "classes": false
+      }
+    ],
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      {
+        "allowExpressions": true,
+        "allowTypedFunctionExpressions": true,
+        "allowHigherOrderFunctions": false
+      }
+    ],
+    "@typescript-eslint/ban-types": "off",
+    "camelcase": "off"
+  },
+  "overrides": [
+    {
+      "files": ["*.spec.ts"],
+      "rules": {
+        "@typescript-eslint/camelcase": "off",
+        "@typescript-eslint/no-explicit-any": "warn"
+      }
+    }
+  ]
 };
